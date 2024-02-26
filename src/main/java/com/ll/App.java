819a2f948638733095b4d97effc5909898382874
gykim93 <gykim1993@gmail.com>
2024-02-26 19:26:59 +0900

    3단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -4,14 +4,12 @@ import java.util.Scanner;
 
 class App {
   void run() {
-
     System.out.println("== 명언 앱 ==");
     while (true) {
       System.out.print("명령) ");
 
       Scanner scanner = new Scanner(System.in);
       String cmd = scanner.nextLine();
-
 
       if(cmd.equals("종료")){
         break;
@@ -23,7 +21,7 @@ class App {
         System.out.print("작가 : ");
         String authorName = scanner.nextLine();
 
-        System.out.printf("명언 : %s, 작가 : %s\n", content, authorName);
+        System.out.println("1번 명언이 등록되었습니다.");
       }
     }
   }

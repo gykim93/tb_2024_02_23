e0a432c493f0b9a082b43ed1394b6cb9cefd03e6
gykim93 <gykim1993@gmail.com>
2024-02-26 21:36:41 +0900

    5단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -16,10 +16,9 @@ class App {
       Scanner scanner = new Scanner(System.in);
       String cmd = scanner.nextLine();
 
-      if(cmd.equals("종료")){
+      if (cmd.equals("종료")) {
         break;
-      }
-      else if (cmd.equals("등록")) {
+      } else if (cmd.equals("등록")) {
         System.out.print("명언 : ");
         String content = scanner.nextLine();
 
@@ -32,8 +31,19 @@ class App {
         quotations.add(quotation);
 
         System.out.printf("%d번 명언이 등록되었습니다.\n", lastQuotationid);
-      } else if(cmd.equals("목록")){
-        System.out.println("총 갯수 : " + quotations.size());
+      } else if (cmd.equals("목록")) {
+        System.out.println("번호 / 작가 / 명언");
+
+        System.out.println("--------------------");
+
+        if (quotations.isEmpty())
+          System.out.println("등록된 명언이 없습니다.");
+
+        for (int i = quotations.size() - 1; i >= 0; i--) {
+          Quotation quotation = quotations.get(i);
+          System.out.printf("%d / %s / %s\n", quotation.id, quotation.authorName, quotation.content);
+        }
+
       }
     }
   }

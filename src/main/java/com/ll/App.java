855c95e88f64f53e695b01d7a85320016a779148
gykim93 <gykim1993@gmail.com>
2024-02-28 21:10:47 +0900

    6단계 시작 cmd 에서 번호를 제외í하고정수화시킴

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -28,11 +28,11 @@ class App {
         break;
       } else if (cmd.equals("등록")) {
         actionWrite();
-
         System.out.printf("%d번 명언이 등록되었습니다.\n", lastQuotationid);
       } else if (cmd.equals("목록")) {
         actionList();
-
+      } else if(cmd.startsWith("삭제?")){
+        actionRemove(cmd);
       }
     }
   }
@@ -62,5 +62,11 @@ class App {
       Quotation quotation = quotations.get(i);
       System.out.printf("%d / %s / %s\n", quotation.id, quotation.authorName, quotation.content);
     }
+  }
+  void actionRemove(String cmd){
+    String idStr = cmd.replace("삭제?id=", "");
+    int id = Integer.parseInt(idStr);
+
+    System.out.printf("%d번 명언을 삭제합니다\n", id);
   }
 }
